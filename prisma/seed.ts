6c04dc7de58f7634d3cc7bf457fbd29953f608d4
chunkygoo <calvintai0402@gmail.com>
2023-01-14 19:08:21 -0800

    added seed script

---
+++ prisma/seed.ts
@@ -0,0 +1,110 @@
+import { faker } from "@faker-js/faker";
+import { MaterialUnit, WeatherCondition } from "@prisma/client";
+import { env } from "../src/env/server.mjs";
+import { prisma } from "../src/server/db";
+
+async function main() {
+  const userId = env.TEST_USER_ID;
+
+  // create 1 project
+  const project = await prisma.project.create({
+    data: {
+      name: faker.company.name(),
+      createdBy: {
+        connect: {
+          id: userId,
+        },
+      },
+    },
+  });
+
+  // create 2 site diaries and some related values
+  for (let i = 0; i < 2; i++) {
+    await prisma.siteDiary.create({
+      data: {
+        name: `Site Diary ${i}`,
+        date: new Date(),
+        createdBy: {
+          connect: {
+            id: userId,
+          },
+        },
+        project: {
+          connect: {
+            id: project.id,
+          },
+        },
+        plants: {
+          create: {
+            type: "Laborer type",
+            amount: Number(faker.random.numeric(1)),
+            createdBy: {
+              connect: {
+                id: userId,
+              },
+            },
+          },
+        },
+        weather: {
+          create: {
+            morning: WeatherCondition.SUNNY,
+            afternoon: WeatherCondition.RAINING,
+            evening: WeatherCondition.CLOUDY,
+          },
+        },
+        laborers: {
+          create: {
+            type: "Laborer type",
+            amount: Number(faker.random.numeric(1)),
+            createdBy: {
+              connect: {
+                id: userId,
+              },
+            },
+          },
+        },
+        materials: {
+          create: {
+            units: MaterialUnit.NR,
+            amount: Number(faker.random.numeric(1)),
+            createdBy: {
+              connect: {
+                id: userId,
+              },
+            },
+          },
+        },
+        siteProblems: {
+          create: {
+            comments: faker.random.words(4),
+            createdBy: {
+              connect: {
+                id: userId,
+              },
+            },
+          },
+        },
+        workProgresses: {
+          create: {
+            comments: faker.random.words(4),
+            createdBy: {
+              connect: {
+                id: userId,
+              },
+            },
+          },
+        },
+      },
+    });
+  }
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
